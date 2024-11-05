29f367fe47f8e12dfde51f3d196fe6120c1b7754
joaorogante <143849096+joaorogante@users.noreply.github.com>
2024-11-05 16:22:17 -0300

    Update BancoCripto.java

--- src/bancocripto/BancoCripto.java
+++ src/bancocripto/BancoCripto.java
@@ -7,11 +7,10 @@ import view.FrameCadastro;
  *
  * @author João
  */
+
 public class BancoCripto {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
        
         FrameCadastro  jc = new FrameCadastro();
